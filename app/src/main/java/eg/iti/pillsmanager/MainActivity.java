a924be4be783d334c6503aaa7ba11a2de2e9bf2e
Ali Alsayed <ali.alsayed9519@gmail.com>
2022-02-16 18:34:41 +0200

    create splash screen ,longin screen and added calender to hame screen

--- app/src/main/java/eg/iti/pillsmanager/MainActivity.java
+++ app/src/main/java/eg/iti/pillsmanager/MainActivity.java
@@ -1,8 +1,15 @@
 package eg.iti.pillsmanager;
 
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Calendar;
+
+import devs.mulham.horizontalcalendar.HorizontalCalendar;
+import devs.mulham.horizontalcalendar.utils.HorizontalCalendarListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /* starts before 1 month from now */
+        Calendar startDate = Calendar.getInstance();
+        startDate.add(Calendar.MONTH, -1);
+
+        /* ends after 1 month from now */
+        Calendar endDate = Calendar.getInstance();
+        endDate.add(Calendar.MONTH, 1);
+
+        // on below line we are setting up our horizontal calendar view and passing id our calendar view to it.
+        HorizontalCalendar horizontalCalendar = new HorizontalCalendar.Builder(this, R.id.calendarView)
+                // on below line we are adding a range
+                // as start date and end date to our calendar.
+                .range(startDate, endDate)
+                // on below line we are providing a number of dates
+                // which will be visible on the screen at a time.
+                .datesNumberOnScreen(5)
+                // at last we are calling a build method
+                // to build our horizontal recycler view.
+                .build();
+        // on below line we are setting calendar listener to our calendar view.
+        horizontalCalendar.setCalendarListener(new HorizontalCalendarListener() {
+            @Override
+            public void onDateSelected(Calendar date, int position) {
+                // on below line we are printing date
+                // in the logcat which is selected.
+                Log.i("TAG", "CURRENT DATE IS " + date);
+            }
+        });
     }
 }
--- app/src/main/java/eg/iti/pillsmanager/SplashActivity.java
+++ app/src/main/java/eg/iti/pillsmanager/SplashActivity.java
@@ -2,7 +2,9 @@ package eg.iti.pillsmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -10,5 +12,16 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        splash();
+    }
+
+    private void splash(){
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, LoginAtivity.class));
+            }
+        }, 2000);
     }
 }
