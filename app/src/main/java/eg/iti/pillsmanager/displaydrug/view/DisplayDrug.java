94d13eb8eb7c46fa7bfbd95bb8f8af83bfdaf351
TheBoss000 <Bossmail28@gmail.com>
2022-02-18 11:29:56 +0200

    added bottom navigation bar and it's fragments as empty fragments for farther use && added medication incomplete view screen layout UI

---
+++ app/src/main/java/eg/iti/pillsmanager/displaydrug/view/DisplayDrug.java
@@ -0,0 +1,16 @@
+package eg.iti.pillsmanager.displaydrug.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import eg.iti.pillsmanager.R;
+
+public class DisplayDrug extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_drug);
+    }
+}
