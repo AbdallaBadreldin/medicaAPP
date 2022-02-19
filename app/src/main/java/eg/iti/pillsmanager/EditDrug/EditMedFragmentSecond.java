b3e6039bfc20bd1d31f73e69d46025931f01700f
esraakhaled <www.esraa.khaled.fciscusuez@gmail.com>
2022-02-19 21:46:25 +0200

    add med screen and edit med screen

--- app/src/main/java/eg/iti/pillsmanager/EditDrug/EditMedFragmentSecond.java
+++ app/src/main/java/eg/iti/pillsmanager/EditDrug/EditMedFragmentSecond.java
@@ -4,62 +4,40 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
 
 import eg.iti.pillsmanager.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link EditMedFragmentSecond#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class EditMedFragmentSecond extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public EditMedFragmentSecond() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment EditMedFragment2.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static EditMedFragmentSecond newInstance(String param1, String param2) {
-        EditMedFragmentSecond fragment = new EditMedFragmentSecond();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+  Button back_btn,next_btn;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_edit_med_second, container, false);
+        View view = inflater.inflate(R.layout.fragment_edit_med_second, container, false);
+        back_btn = view.findViewById(R.id.btn_back_add_two);
+        next_btn = view.findViewById(R.id.btn_next_add_second);
+        back_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+             Edit_medActivity.showFragmentone();
+            }
+        });
+        next_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Edit_medActivity.showFragmentThree();
+            }
+        });
+        return view;
     }
 }
--- app/src/main/java/eg/iti/pillsmanager/EditDrug/Edit_medActivity.java
+++ app/src/main/java/eg/iti/pillsmanager/EditDrug/Edit_medActivity.java
@@ -3,16 +3,45 @@ package eg.iti.pillsmanager.EditDrug;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import eg.iti.pillsmanager.R;
+import eg.iti.pillsmanager.addDrug.view.AddMedFragmentSecond;
+import eg.iti.pillsmanager.addDrug.view.AddMedFragmentThree;
+import eg.iti.pillsmanager.addDrug.view.AddMedicationFragment;
 
 public class Edit_medActivity extends AppCompatActivity {
+    private static FragmentManager fragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_med);
+        fragmentManager = getSupportFragmentManager();
+        showFragmentone();
 
 
+    }
+    public static void showFragmentone(){
+        EditMedFragmentOne editMedicationFragment = new EditMedFragmentOne();
+        showFragment( editMedicationFragment);
+    }
+
+    public static void showFragmentSecond(){
+        EditMedFragmentSecond editMedFragmentSecond = new EditMedFragmentSecond();
+        showFragment(editMedFragmentSecond );
+    }
+
+    public static void showFragmentThree(){
+        EditMedFragmentThree editMedFragmentThree = new EditMedFragmentThree();
+        showFragment(editMedFragmentThree);
+    }
+
+    private static void showFragment(Fragment fragment){
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.main_activity_container, fragment);
+        fragmentTransaction.commit();
     }
 }
