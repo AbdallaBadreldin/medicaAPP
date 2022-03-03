574b3b852fcb34908f703bcc7b6e035deb0995f8
TheBoss000 <Bossmail28@gmail.com>
2022-03-03 14:19:55 +0200

    now you  can login and log out fine also you cannot enter large numbers in refill and doses beside some changes in colors and user experiences

--- app/src/main/java/eg/iti/pillsmanager/auth/singnIn/view/SigninFragment.java
+++ app/src/main/java/eg/iti/pillsmanager/auth/singnIn/view/SigninFragment.java
@@ -1,7 +1,12 @@
 package eg.iti.pillsmanager.auth.singnIn.view;
 
+import static android.content.Intent.FLAG_ACTIVITY_REORDER_TO_FRONT;
+
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,10 +14,12 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
 import androidx.fragment.app.Fragment;
-import eg.iti.pillsmanager.auth.AuthAsyncCallBackI;
+
 import eg.iti.pillsmanager.MainActivity;
 import eg.iti.pillsmanager.R;
+import eg.iti.pillsmanager.auth.AuthAsyncCallBackI;
 import eg.iti.pillsmanager.auth.singnIn.presenter.SignInPresenter;
 
 public class SigninFragment extends Fragment implements AuthAsyncCallBackI {
@@ -21,10 +28,14 @@ public class SigninFragment extends Fragment implements AuthAsyncCallBackI {
     Button signInBtn;
     SigninFragment signinFragment;
     SignInPresenter signInPresenter;
+    SharedPreferences sharedPreferences;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        sharedPreferences = getActivity().getSharedPreferences("sh", Context.MODE_PRIVATE);
+
 
         signInPresenter = new SignInPresenter();
         signinFragment = this;
@@ -75,7 +86,11 @@ public class SigninFragment extends Fragment implements AuthAsyncCallBackI {
 
     @Override
     public void onSuccess(String actionType) {
-        startActivity(new Intent(getActivity(), MainActivity.class ));
+       sharedPreferences= PreferenceManager.getDefaultSharedPreferences(getContext());
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("email",signInEmail.getText().toString().trim());
+        editor.apply();
+        startActivity(new Intent(getActivity(), MainActivity.class ).addFlags(FLAG_ACTIVITY_REORDER_TO_FRONT));
     }
 
     @Override
